b9e2478114ef9cd27f6d12b37a27771cd0687184
Aurora Ziobrowski <aziobrow@gmail.com>
2018-06-29 21:38:29 -0600

    updated initializer spec to reflect initializer updates

--- spec/lib/services/dictionary_initializer_spec.rb
+++ spec/lib/services/dictionary_initializer_spec.rb
@@ -1,15 +1,26 @@
 require 'rails_helper'
 
 describe DictionaryInitializer do
-  let(:initializer)  { DictionaryInitializer.new }
-
   it "intializes with default text file" do
+    initializer = DictionaryInitializer.new
+
     expect(initializer.txt_file).to eq('dictionary.txt')
   end
 
-  it "converts rows of text into words" do
-    expect {
+  context 'when reading lines of file' do
+    let(:initializer) { DictionaryInitializer.new('spec/fixtures/test_words.txt') }
+
+    it "converts lines into words" do
+      expect {
+        initializer.create_dictionary
+      }.to change(Word, :count).from(0).to(3)
+    end
+
+    it "converts words to lowercase" do
       initializer.create_dictionary
-    }.to change(Word, :count).from(0)
+
+      expect(Word.all).to include('zebra')
+      expect(Word.all).not_to include('Zebra')
+    end
   end
 end

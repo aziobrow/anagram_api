8f451cc889377ab0f11cbfd74714ed9522a8fe5d
Aurora Ziobrowski <aziobrow@gmail.com>
2018-06-29 21:37:52 -0600

    Updated dictionary initializer to include default text file

--- lib/services/dictionary_initializer.rb
+++ lib/services/dictionary_initializer.rb
@@ -1,15 +1,15 @@
 class DictionaryInitializer
   attr_accessor :txt_file
 
-  def initialize
-    @txt_file = 'dictionary.txt'
+  def initialize(txt_file = 'dictionary.txt')
+    @txt_file = txt_file
   end
 
   def create_dictionary
     count = 0
 
     File.open(txt_file). each do |line|
-      Word.create!(word: line)
+      Word.create!(word: line.downcase)
 
       puts "Word #{count} created"
       count += 1

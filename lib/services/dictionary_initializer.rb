31ab512d6b61cc8e0f19dcf3eb124f028a51add2
Aurora Ziobrowski <aziobrow@gmail.com>
2018-06-30 18:04:54 -0600

    Updated to class methods and added action to expose creation of new words

--- lib/services/dictionary_initializer.rb
+++ lib/services/dictionary_initializer.rb
@@ -1,18 +1,16 @@
 class DictionaryInitializer
-  attr_accessor :txt_file
-
-  def initialize(txt_file = 'dictionary.txt')
-    @txt_file = txt_file
-  end
-
-  def create_dictionary
+  def self.create_dictionary(txt_file = 'dictionary.txt')
     count = 0
 
     File.open(txt_file). each do |line|
-      Word.create!(word: line.strip.downcase)
+      Word.create(word: line.strip.downcase)
 
       puts "Word #{count} created"
       count += 1
     end
+  end
+
+  def self.add_word(word)
+    Word.create(word: word.downcase)
   end
 end

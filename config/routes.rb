80f0fc33e04f8b7518ed7ae97209c18919f006f9
Aurora Ziobrowski <aziobrow@gmail.com>
2018-06-30 22:09:27 -0600

    added routes for destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :words, only: [:create]
+      resources :words, param: :word, only: [:create, :destroy] do
+        collection do
+          delete :destroy
+        end
+      end
     end
   end
 end

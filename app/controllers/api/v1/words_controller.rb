7c55a4d77262723f8435907104c26eaee5d2d4a4
Aurora Ziobrowski <aziobrow@gmail.com>
2018-06-30 18:05:38 -0600

    Added api words controller

---
+++ app/controllers/api/v1/words_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::WordsController < ApplicationController
+
+  def create
+    creator = WordsCreator.new
+
+    ActiveRecord::Base.transaction do
+      creator.create_words!(word_params)
+      render json: creator.successes
+    end
+
+  rescue StandardError
+    render status: :unprocessable_entity, json: creator.errors
+  end
+
+  private
+
+  def word_params
+    params.require(:words)
+  end
+end

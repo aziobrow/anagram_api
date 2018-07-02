0c35789e8219ce5cb4d821ef709a2375552bc59b
aziobrow <aziobrow@gmail.com>
2018-07-02 16:18:44 -0600

    add controller action for word analytics

--- app/controllers/api/v1/words_controller.rb
+++ app/controllers/api/v1/words_controller.rb
@@ -20,6 +20,29 @@ class Api::V1::WordsController < ApplicationController
     end
   end
 
+  def analytics
+    total = Word.all.size
+
+    return render json: { total_words: 0 } if total.zero?
+
+    min = Word.pluck('MIN(LENGTH(word))').first
+    max = Word.pluck('MAX(LENGTH(word))').first
+    median = 10
+    avg = Word.pluck('AVG(LENGTH(word))').first
+
+    render json: {
+      analytics:  {
+        total_words: total,
+        word_length:  {
+          minimum: min,
+          maximum: max,
+          median: median,
+          average: avg
+        }
+      }
+    }
+  end
+
   private
 
   def word_params

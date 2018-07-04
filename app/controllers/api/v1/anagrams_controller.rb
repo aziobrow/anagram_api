e6a14b9ca447a0a20e0e3d4d23e71f1128a53074
Aurora Ziobrowski <aziobrow@gmail.com>
2018-07-04 15:30:32 -0600

    hooked up controller actions to anagram routes

--- app/controllers/api/v1/anagrams_controller.rb
+++ app/controllers/api/v1/anagrams_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AnagramsController < ApplicationController
-  before_action :load_handler
+  before_action :load_handler, only: [:show, :destroy]
 
   def show
     @anagrams.delete(params[:word])
@@ -8,6 +8,22 @@ class Api::V1::AnagramsController < ApplicationController
     render json: {
       anagrams: @anagrams
     }
+  end
+
+  def verify_set
+    render json: { anagrams?: AnagramHandler.new.anagrams?(params[:words]) }
+  end
+
+  def groups
+    if params[:largest]
+      largest_group = AnagramHandler.new.most_anagrams if params[:largest]
+      render json: { words_with_most_anagrams: largest_group }
+    elsif params[:size]
+      eligible_groups = AnagramHandler.new.anagram_groups_of_x_size(params[:size].to_i)
+      render json: { eligible_groups: eligible_groups }
+    else
+      render status: :unprocessable_entity
+    end
   end
 
   def destroy

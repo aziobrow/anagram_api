599c830d12dc16d5ce570d245b5dc9ef3227412f
Aurora Ziobrowski <aziobrow@gmail.com>
2018-07-03 21:36:08 -0600

    return empty array for no anagram result

--- lib/services/anagram_handler.rb
+++ lib/services/anagram_handler.rb
@@ -9,7 +9,7 @@ class AnagramHandler
     word_chars = word.downcase.chars.sort
     result = anagrams[word_chars]
 
-    return unless result.present?
+    return [] unless result.present?
 
     result.delete(word)
     result

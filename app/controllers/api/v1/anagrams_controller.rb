15715684f7f8b1575e99c16e6f8c30b19dc17717
Aurora Ziobrowski <aziobrow@gmail.com>
2018-07-03 23:34:20 -0600

    method to delete a word and its anagrams

--- app/controllers/api/v1/anagrams_controller.rb
+++ app/controllers/api/v1/anagrams_controller.rb
@@ -1,11 +1,23 @@
 class Api::V1::AnagramsController < ApplicationController
-  def show
-    anagrams = AnagramHandler.new.find_anagrams(params[:word])
+  before_action :load_handler
 
-    anagrams = anagrams.first(params[:limit].to_f) if params[:limit]
+  def show
+    @anagrams = @anagrams.first(params[:limit].to_f) if params[:limit]
 
     render json: {
-      anagrams: anagrams
+      anagrams: @anagrams
     }
+  end
+
+  def destroy
+    words_to_delete = @anagrams << params[:word]
+
+    Word.where(word: words_to_delete).delete_all
+  end
+
+  private
+
+  def load_handler
+    @anagrams = AnagramHandler.new.find_anagrams(params[:word])
   end
 end
